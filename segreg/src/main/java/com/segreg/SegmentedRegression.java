b880e8157171ec8d87d0d7cd05ed5c5fd7b69c8b
TaeYoung Kim <hihikim92@gmail.com>
2025-12-09 17:42:30 +0900

    save

--- segreg/src/main/java/com/segreg/SegmentedRegression.java
+++ segreg/src/main/java/com/segreg/SegmentedRegression.java
@@ -3,12 +3,14 @@ package com.segreg;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 import org.apache.commons.math3.stat.regression.RegressionResults;
 import java.util.Deque;
+import java.util.Vector;
 
 public class SegmentedRegression {
     private double [] xData;
     private double [] yData;
     private SimpleRegression regression;
     private Deque<int[]> segmentStack;
+    private int SMAWindowLen = 10; // 이동 평균 윈도우의 길이
 
     public SegmentedRegression(double[] xData, double[] yData) {
         this.xData = xData;
@@ -54,53 +56,94 @@ public class SegmentedRegression {
         int[] range = new int[]{0, xData.length};
 
         segmentStack.add(range);
+
+        double[] smadata = CalculateSMA(SMAWindowLen);
+
+        double[] deviations = new double[yData.length];
+
+        for (int i = 0; i < yData.length; i++) {
+            deviations[i] = Math.abs(yData[i] - smadata[i]);
+        }
+
+        //  find positions of local maxima in the vector of deviations 
+        Vector<Integer> vec_max_indices = new Vector<Integer>(); 
+        FindLocalMaxima (deviations, vec_max_indices) ; 
+
         while (!segmentStack.isEmpty() ){
             int[] currentRange = segmentStack.pop();
             // Logic to find the best split point and add new segments to the stack
             // This is a placeholder for the actual segmentation logic
-            if 
+
+            // TODO: Implement segmentation based on local maxima and maxSegments
         }
         for (int i = 0; i < xData.length; i++) {
             regression.addData(xData[i], yData[i]);
-        }
-
-
-
-        
+        }        
         return new RegressionResults[] { regression.regress() };
+    }
+
+    public void FindLocalMaxima(double[] data, Vector<Integer> vec_max_indices) {
+        int n = data.length;
+        
+        if (n < 3) {
+            return;
+        }
+        
+        for (int i = 1; i < n - 1; i++) {
+            if (data[i] > data[i - 1] && data[i] > data[i + 1]) {
+                vec_max_indices.add(i);
+            }
+        }
     }
 
     public boolean CanSplitSegment(int[] segment,  ) {
         // Placeholder logic for determining if a segment can be split
         return (endIndex - startIndex) > 2; // Example condition
+        // TODO: Implement actual logic based on regression error or other criteria
     }
 
-    public double[] CalculateSMA(int[] range, int half_len){
-        int n_values = range[1] - range[0];
+    public double[] CalculateSMA(int half_len){
+        int n_values = yData.length;
         double[] result = new double[n_values];
-        System.arraycopy(yData, range[0], result, 0, n_values);
+        System.arraycopy(yData, 0, result, 0, n_values);
         if ( half_len<=0 || n_values<3 ) {
         }
+
         else if( ( 2*half_len + 1 ) > n_values ) {
         }
+
         else{
             int ix = 0; 
             double sum_y = 0.0;
 
             sum_y = result[0];
-            result[0] = sum_y / 1.0  ;
+            for (int i = 1; i <= half_len; i++) {
+                sum_y = sum_y + yData[i];
+            }
+            result[0] = sum_y / (half_len + 1);
 
             //  the front range:
             //  processing accumulates sum_y using gradually increasing length window
 
-            for (int i = range[0]; i < half_len; i++) {
-                sum_y = sum_y + yData[range[0] + i][ 2*ix - 1 ] + data_copy [ 2*ix ] ;
+            for (int i = 1; i <= half_len; i++) {
+                sum_y = sum_y + yData[i];
+                result[i] = sum_y / (half_len + 1 + i);
+            }
+
+            for (int i = half_len + 1; i < n_values - half_len; i++) {
+                sum_y = sum_y + yData[i] - yData[i - half_len - 1];
+                result[i] = sum_y / (2 * half_len + 1);
+            }
+            //  the back range:
+            //  processing accumulates sum_y using gradually decreasing length window
+
+            for (int i = n_values - half_len; i < n_values; i++) {
+                sum_y = sum_y - yData[i - half_len - 1];
+                result[i] = sum_y / (half_len + n_values - i);
             }
         }
         return result;
-    }
-
-
+    }    
 }
 
 
