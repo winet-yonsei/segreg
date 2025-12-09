dfd1b0fb5c98b736631f3bf747139523bfe7e9f9
TaeYoung Kim <hihikim92@gmail.com>
2025-12-09 17:21:02 +0900

    save data

--- segreg/src/main/java/com/segreg/Main.java
+++ segreg/src/main/java/com/segreg/Main.java
@@ -1,7 +1,11 @@
 package com.segreg;
 
+import org.jfree.chart.ChartFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("input max segments:");
+        int maxSegments = Integer.parseInt(System.console().readLine());
+        SegmentedRegression sr = new SegmentedRegression(new double[]{1,2,3,4,5}, new double[]{2,3,5,7,11});
     }
 }
--- segreg/src/main/java/com/segreg/SegmentedRegression.java
+++ segreg/src/main/java/com/segreg/SegmentedRegression.java
@@ -2,11 +2,13 @@ package com.segreg;
 
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 import org.apache.commons.math3.stat.regression.RegressionResults;
+import java.util.Deque;
 
 public class SegmentedRegression {
     private double [] xData;
     private double [] yData;
     private SimpleRegression regression;
+    private Deque<int[]> segmentStack;
 
     public SegmentedRegression(double[] xData, double[] yData) {
         this.xData = xData;
@@ -35,8 +37,8 @@ public class SegmentedRegression {
         System.arraycopy(yData, 0, newYData, 0, yData.length);
         newXData[xData.length] = x;
         newYData[yData.length] = y;
-        this.xData = newXData;
-        this.yData = newYData;
+        xData = newXData;
+        yData = newYData;
     }
 
     public void setData(double[] xData, double[] yData) {
@@ -44,17 +46,58 @@ public class SegmentedRegression {
         this.yData = yData;
     }   
 
+
     public RegressionResults[] performRegression(int maxSegments) {
-        return this.performRegression(maxSegments, 0, xData.length);
-    }
-
-
-    public RegressionResults[] performRegression(int maxSegments, int startIndex, int endIndex) {
         regression.clear();
-        for (int i = startIndex; i < endIndex; i++) {
+        segmentStack.clear();
+
+        int[] range = new int[]{0, xData.length};
+
+        segmentStack.add(range);
+        while (!segmentStack.isEmpty() ){
+            int[] currentRange = segmentStack.pop();
+            // Logic to find the best split point and add new segments to the stack
+            // This is a placeholder for the actual segmentation logic
+            if 
+        }
+        for (int i = 0; i < xData.length; i++) {
             regression.addData(xData[i], yData[i]);
         }
+
+
+
+        
         return new RegressionResults[] { regression.regress() };
+    }
+
+    public boolean CanSplitSegment(int[] segment,  ) {
+        // Placeholder logic for determining if a segment can be split
+        return (endIndex - startIndex) > 2; // Example condition
+    }
+
+    public double[] CalculateSMA(int[] range, int half_len){
+        int n_values = range[1] - range[0];
+        double[] result = new double[n_values];
+        System.arraycopy(yData, range[0], result, 0, n_values);
+        if ( half_len<=0 || n_values<3 ) {
+        }
+        else if( ( 2*half_len + 1 ) > n_values ) {
+        }
+        else{
+            int ix = 0; 
+            double sum_y = 0.0;
+
+            sum_y = result[0];
+            result[0] = sum_y / 1.0  ;
+
+            //  the front range:
+            //  processing accumulates sum_y using gradually increasing length window
+
+            for (int i = range[0]; i < half_len; i++) {
+                sum_y = sum_y + yData[range[0] + i][ 2*ix - 1 ] + data_copy [ 2*ix ] ;
+            }
+        }
+        return result;
     }
 
 
